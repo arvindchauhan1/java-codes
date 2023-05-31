051771f494d475ea7fece1c49976a2a9dd061500
ArvindChauhan1 <arvindchn1@gmail.com>
2023-05-31 20:03:20 +0530

    Exception Handling

---
+++ exception-handeling/ArithmeticExp.java
@@ -0,0 +1,11 @@
+public class ArithmeticExp {
+    public static void main(String[] args) {
+        try {
+            int dividebyzero = 5 / 0;
+            System.out.println(dividebyzero);
+        } catch (ArithmeticException e) {
+            System.out.println("ArithmeticException = " + e.getMessage());
+        }
+
+    }
+}
